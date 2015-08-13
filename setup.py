b04cce5f23c4fe7a377ef0d46d9ce8b337eb6869
jack <jack@bancast.net>
2015-08-13 14:45:50 +0100

    Minor update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='xtralien',
     version='0.0.1',
-    description='A connector to implement connecting to CLOI-based instruments',
+    description='A extension to CLOI to allow direct connections to ',
     author='Xtralien',
     author_email='jack@xtralien.com',
     url='https://github.com/Xtralien/pyxtralien.git',
