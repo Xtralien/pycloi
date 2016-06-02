b90b0e94b884998c39096167e360d4a3fdbc9d57
Jack Stephenson <jack@bancast.net>
2016-06-02 14:37:35 +0100

    Re-initial commit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='xtralien',
-    version='0.0.1',
-    description='A extension to CLOI to allow direct connections to ',
+    name='cloi',
+    version='1.0.0',
+    description='A wrapper to the xtralien library',
     author='Xtralien',
     author_email='jack@xtralien.com',
-    url='https://github.com/Xtralien/pyxtralien.git',
-    packages=['xtralien'],
-    install_requires=['cloi'],
+    url='https://github.com/Xtralien/pycloi.git',
+    packages=['cloi'],
+    install_requires=['xtralien'],
     license='GPLv3'
 )
